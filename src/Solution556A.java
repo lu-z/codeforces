81509f44d3d694e82dbe9a12e94044791634d116
Luca Zuccarini <lzuccarini@twitter.com>
2016-11-02 16:39:10 -0400

    First few solutions.

---
+++ src/Solution556A.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+/**
+ * Case of the Zeros and Ones -- http://codeforces.com/problemset/problem/556/A
+ */
+public class Solution556A {
+
+    private static int removePairs(int length, String input) {
+        Stack<Character> processed = new Stack<>();
+
+        for (char current : input.toCharArray()) {
+            if (processed.isEmpty()) {
+                processed.push(current);
+            } else if (processed.peek() != current) {
+                length -= 2;
+                processed.pop();
+            } else {
+                processed.push(current);
+            }
+        }
+
+        return length;
+    }
+
+    public static void main(String args[]) throws Exception {
+        final Scanner scanner = new Scanner(System.in);
+        final int strLen = scanner.nextInt();
+        final String binary = scanner.next();
+        System.out.println(removePairs(strLen, binary));
+    }
+}
---
+++ src/Solution705A.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *  Hulk -- http://codeforces.com/problemset/problem/705/A
+ */
+public class Solution705A {
+
+    private static final Map<Integer, String> sentimentMap = new HashMap<>();
+    static {
+        sentimentMap.put(0, "I hate");
+        sentimentMap.put(1, "I love");
+    }
+
+    private static String getHulkSentiment(int sentiment) {
+        final StringBuilder sb = new StringBuilder();
+        for (int layer = 0; layer < sentiment; layer++) {
+            sb.append(sentimentMap.get(layer % 2));
+            if (layer == sentiment - 1) {
+                sb.append(" it");
+            } else {
+                sb.append(" that ");
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        final Scanner scanner = new Scanner(System.in);
+        System.out.println(getHulkSentiment(scanner.nextInt()));
+    }
+}
